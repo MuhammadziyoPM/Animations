a8e5da111a2abaa6ffdf37778a4848c34d3093e6
Youtube <muhammadziyonps@gmail.com>
2023-04-14 12:42:41 +0500

    This is a Instagram SignUp , SignIn homework

--- app/src/main/java/com/example/animations/SignUpActivity.kt
+++ app/src/main/java/com/example/animations/SignUpActivity.kt
@@ -10,12 +10,12 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        var editText3 = findViewById<Button>(R.id.sign_in)
+        var editText3 = findViewById<Button>(R.id.sign_up2)
         editText3.setOnClickListener {
             val intent = Intent(this@SignUpActivity , SignInActivity ::class.java)
             startActivity(intent)
         }
-        var textView = findViewById<TextView>(R.id.sign_up)
+        var textView = findViewById<TextView>(R.id.sign_in2)
         textView.setOnClickListener {
             val intent2 = Intent(this@SignUpActivity , SignInActivity ::class.java)
             startActivity(intent2)
